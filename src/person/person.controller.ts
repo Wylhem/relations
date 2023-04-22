1ddb8a748b2f973cefbe53030febff8cd15cb21e
Wylhem <wylhemdorv@gmail.com>
2023-04-22 12:01:52 +0200

    :sparkles: Feat:(Person): Crud Person

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,7 +1,92 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PersonService } from './person.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Person } from './entity/person.entitiy';
+import { PersonDto } from './dto/person.dto';
+import { person } from '@prisma/client';
 
+@ApiBearerAuth()
+@ApiTags('Person')
 @Controller('person')
 export class PersonController {
   constructor(private readonly personService: PersonService) {}
+
+  /**
+   * Get All Persons.
+   */
+  @Get()
+  public async getAll(): Promise<Array<PersonDto>> {
+    const persons: Array<Person> = await this.personService.getAll();
+    return persons.map((person) => PersonDto.Load(person));
+  }
+
+  /**
+   * Get One persons with specific id.
+   * @param id
+   * @constructor
+   */
+  @Get(':id')
+  public async GetOne(@Param('id') id: string): Promise<PersonDto> {
+    const person: Person = await this.personService.getOne(id);
+    return PersonDto.Load(person);
+  }
+
+  /**
+   * Create new person
+   * @param personDto
+   */
+  @Post()
+  public async create(@Body() personDto: PersonDto): Promise<PersonDto> {
+    if (!personDto) {
+      throw new BadRequestException();
+    }
+    const person: Person = await this.personService.create(personDto);
+    return PersonDto.Load(person);
+  }
+
+  /**
+   * Connect new person to an existent users
+   * @param personDto
+   */
+  @Patch('/users')
+  public async connectUsers(@Body() personDto: PersonDto): Promise<PersonDto> {
+    if (!personDto) {
+      throw new BadRequestException();
+    }
+    const person: Person = await this.personService.connect(personDto);
+    return PersonDto.Load(person);
+  }
+
+  /**
+   * Update an existent person.
+   * @param personDto
+   */
+  @Put()
+  public async update(@Body() personDto: PersonDto): Promise<PersonDto> {
+    if (!personDto) {
+      throw new BadRequestException();
+    }
+    const person: Person = await this.personService.update(personDto);
+    return PersonDto.Load(person);
+  }
+
+  /**
+   * Delete an existent person
+   * @param id
+   */
+  @Delete(':id')
+  public async delete(@Param('id') id: string): Promise<string> {
+    const person: Person = await this.personService.delete(id);
+    return person.per_id;
+  }
 }
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,6 +1,8 @@
 import { BaseDto } from '../../shared/base/base.dto';
 import { Users } from '../entities/user.entity';
 import { IsEmail } from 'class-validator';
+import { PersonDto } from '../../person/dto/person.dto';
+import { person } from '@prisma/client';
 export class UserDto extends BaseDto {
   /**
    * Gets or sets email.
@@ -23,12 +25,18 @@ export class UserDto extends BaseDto {
    */
   refreshToken: string;
 
+  /**
+   * Gets or sets Person.
+   */
+  person?: PersonDto;
+
   public static Load(users: Users): UserDto {
     return {
       id: users.usr_id,
       username: users.usr_username,
       email: users.usr_email,
       refreshToken: users.usr_refreshToken,
+      person: users.person ? PersonDto.Load(users.person) : null,
       createdAt: users.usr_createdAt,
       updatedAt: users.usr_updatedAt,
     };
