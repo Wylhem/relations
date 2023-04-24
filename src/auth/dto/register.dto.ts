a9373e4853d5c1814094bc47446b1e4323cb1eba
Wylhem <wylhemdorv@gmail.com>
2023-04-24 17:01:47 +0200

    checkout is not empty

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,9 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
 import { AuthDto } from './auth.dto';
+import { IsNotEmpty } from 'class-validator';
 
 export class RegisterDto extends PartialType(AuthDto) {
   /**
    * Gets or sets username.
    */
+  @IsNotEmpty()
   username: string;
 }
